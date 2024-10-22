511640dc70297448457a1426411118da83e32913
Hp <Hp@DESKTOP-P3JKKI0>
2024-10-22 10:24:32 +0700

    First Commit to start the project

---
+++ Object Repository/Page_LoginIBB/hyperLink_ForgotPassword.rs
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>hyperLink_ForgotPassword</name>
+   <tag></tag>
+   <elementGuidId>5515e5f2-9fcb-4352-bc7f-7ea2f2814d9d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[contains(text(), 'Forgot Password?') or contains(text(), 'Lupa Kata Sandi?')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>62b36eec-f054-474d-80e7-5218e62c45e4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>MuiTypography-root MuiTypography-body2 a-text-style #0061A7 400 css-yykrz5</value>
+      <webElementGuid>989887d6-0e3b-4729-90ca-ece9ce220b54</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Lupa Kata Sandi?</value>
+      <webElementGuid>03b78715-e6fd-414c-b5ff-5d83e26ee668</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;MuiScopedCssBaseline-root css-y57rlh&quot;]/div[@class=&quot;MuiBox-root css-0&quot;]/div[@class=&quot;css-ietzmr&quot;]/div[@class=&quot;css-y09veg&quot;]/div[@class=&quot;MuiGrid-root MuiGrid-container css-18od73n&quot;]/div[@class=&quot;MuiGrid-root MuiGrid-item MuiGrid-grid-xs-4 MuiGrid-grid-sm-8 MuiGrid-grid-md-4.5 css-17eop0f&quot;]/div[@class=&quot;css-j7qwjs&quot;]/div[@class=&quot;css-yqs4zs&quot;]/div[@class=&quot;css-q5371m&quot;]/div[@class=&quot;css-uwwqev&quot;]/form[1]/div[@class=&quot;css-28fbj7&quot;]/button[@class=&quot;MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary MuiButton-disableElevation MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary MuiButton-disableElevation a-button text md text-variant css-45qc2l&quot;]/span[@class=&quot;MuiTypography-root MuiTypography-text css-1uk1gs8&quot;]/p[@class=&quot;MuiTypography-root MuiTypography-body2 a-text-style #0061A7 400 css-yykrz5&quot;]</value>
+      <webElementGuid>d962b870-924e-4955-bd77-8d917b5e263f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div/div[2]/div/div[2]/div[2]/div/div/div/div/form/div[5]/button/span/p</value>
+      <webElementGuid>de4748d2-340d-42b1-ba84-ac0e02178ab2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='​'])[3]/following::p[2]</value>
+      <webElementGuid>8e659c70-c03e-4fac-9ebb-a3a4878796d6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Lupa Kata Sandi?']/parent::*</value>
+      <webElementGuid>2123a579-e1a9-42df-9743-c88a038bf514</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//span/p</value>
+      <webElementGuid>fdb9be51-1423-42bb-864f-81558909341e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'Lupa Kata Sandi?' or . = 'Lupa Kata Sandi?')]</value>
+      <webElementGuid>1574db13-4c6f-4b34-9bf0-9b90fd4d3495</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
